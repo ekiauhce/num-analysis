9444ecdca388f68d9eb842e092f12a42fb8e14e2
Daniil Beliak <daniil-belyak@yandex-team.ru>
2022-10-23 18:03:45 +0300

    two variants

--- gauss_t.c
+++ gauss_t.c
@@ -17,6 +17,9 @@ void run_tests(struct method* method);
 void _round_test();
 
 
+const int VARIANT_3 = 3;
+const int VARIANT_16 = 16;
+
 int main(int argc, char const *argv[]) {
     mkdir("results", 0777);
     _round_test();
@@ -40,7 +43,7 @@ double _round(double value, int places) {
     return round(value * pow(10, places)) / pow(10, places);
 }
 
-double** get_matrix_with_sqrts(int places) {
+double** get_matrix_with_sqrts(int places, int variant) {
     int n = 4;
     double** a = malloc(sizeof(double*) * n);
 
@@ -48,54 +51,64 @@ double** get_matrix_with_sqrts(int places) {
         a[i] = malloc(sizeof(double) * n);
     }
 
-    // 3 вариант
     // https://miit.ru/content/Содержимое.pdf?id_vf=13922
-    // a[0][0] = 1.;                               a[0][1] = 1/_round(sqrt(2.), places);       a[0][2] =  1/_round(sqrt(2.), places); a[0][3] =  0;
-    // a[1][0] = 7./12;                            a[1][1] = 4./5;                             a[1][2] = -1./3;                       a[1][3] =  1./2;
-    // a[2][0] = 2./3;                             a[2][1] = _round(sqrt(2), places)/3;        a[2][2] = -1./3;                       a[2][3] = -1./_round(sqrt(3), places);
-    // a[3][0] = (1 + _round(sqrt(2), places))/2.; a[3][1] = (2 + _round(sqrt(2), places))/4.; a[3][2] =  0;                          a[3][3] = -_round(sqrt(3), places)/(2*_round(sqrt(2), places));
-
-    a[0][0] = (1 + _round(sqrt(2), places))/2.; a[0][1] = (2 + _round(sqrt(2), places))/4.; a[0][2] =  0;                          a[0][3] = -_round(sqrt(3), places)/(2*_round(sqrt(2), places));
-    a[1][0] = 7./12;                            a[1][1] = 4./5;                             a[1][2] = -1./3;                       a[1][3] =  1./2;
-    a[2][0] = 2./3;                             a[2][1] = _round(sqrt(2), places)/3;        a[2][2] = -1./3;                       a[2][3] = -1./_round(sqrt(3), places);
-    a[3][0] = 1.;                               a[3][1] = 1/_round(sqrt(2.), places);       a[3][2] =  1/_round(sqrt(2.), places); a[3][3] =  0;
-
+    if (variant == VARIANT_3) {
+        a[0][0] = 1.;                               a[0][1] = 1/_round(sqrt(2.), places);       a[0][2] =  1/_round(sqrt(2.), places); a[0][3] =  0;
+        a[1][0] = 7./12;                            a[1][1] = 4./5;                             a[1][2] = -1./3;                       a[1][3] =  1./2;
+        a[2][0] = 2./3;                             a[2][1] = _round(sqrt(2), places)/3;        a[2][2] = -1./3;                       a[2][3] = -1./_round(sqrt(3), places);
+        a[3][0] = (1 + _round(sqrt(2), places))/2.; a[3][1] = (2 + _round(sqrt(2), places))/4.; a[3][2] =  0;                          a[3][3] = -_round(sqrt(3), places)/(2*_round(sqrt(2), places));
+    } else if (variant == VARIANT_16) {
+        a[0][0] = _round(sqrt(5), places);  a[0][1] =  2*_round(sqrt(3), places); a[0][2] =  0.5; a[0][3] = -1.4;
+        a[1][0] = -_round(sqrt(3), places); a[1][1] = -6/_round(sqrt(5), places); a[1][2] =  3.2; a[1][3] =  2.5;
+        a[2][0] = -1.1;                     a[2][1] =  4.2;                       a[2][2] = -3.2; a[2][3] =  0.8;
+        a[3][0] =  7.9;                     a[3][1] = -5.2;                       a[3][2] =  0.3; a[3][3] =  2.9;
+    }
     return a;
 }
 
-double* get_b_for_sqrts_case() {
+double* get_b_for_sqrts_case(int variant) {
     int n = 4;
     double* b = malloc(sizeof(double) * n);
-    b[0] = 0.15;
-    b[1] = 0.09;
-    b[2] = -0.05;
-    b[3] = 0.27;
+
+    if (variant == VARIANT_3) {
+        b[0] = 0.15;
+        b[1] = 0.09;
+        b[2] = -0.05;
+        b[3] = 0.27;
+    } else if (variant == VARIANT_3) {
+        b[0] = 4.5;
+        b[1] = 0.7;
+        b[2] = 0.9;
+        b[3] = 6.2;
+    }
     return b;
 }
 
 
+struct matrix get_matrix_obj(int places, int variant) {
+    struct matrix m = {4, get_matrix_with_sqrts(places, variant), get_b_for_sqrts_case(variant)};
+    return m;
+}
+
 void run_tests(struct method* method) {
-    const char *names[] = {
-        "sqrt2.txt",
-        "sqrt4.txt",
-        "sqrt6.txt",
-        "sqrt10.txt",
-    };
-    struct matrix matrices[] = {
-        {4, get_matrix_with_sqrts(2), get_b_for_sqrts_case()},
-        {4, get_matrix_with_sqrts(4), get_b_for_sqrts_case()},
-        {4, get_matrix_with_sqrts(6), get_b_for_sqrts_case()},
-        {4, get_matrix_with_sqrts(10), get_b_for_sqrts_case()},
+    const int places[] = {
+        2,
+        4,
+        6,
+        10
     };
 
-    for (int i = 0; i < sizeof(matrices) / sizeof(matrices[0]); i++) {
+    const int variants[] = { VARIANT_3, VARIANT_16 };
+
+    for (int i = 0; i < sizeof(variants) / sizeof(int); i++) {
+        for (int j = 0; j < sizeof(places) / sizeof(int); j++) {
         char buf[256];
-        snprintf(buf, sizeof(buf), "results/%s_%s", method->name, names[i]);
+        snprintf(buf, sizeof(buf), "results/%s_sqrt%d_var%d.txt", method->name, places[j], variants[i]);
 
         FILE *f = fopen(buf, "w");
 
-        struct matrix m = matrices[i];
-        fill_aux_fields(&m, get_matrix_with_sqrts(-1));
+        struct matrix m = get_matrix_obj(places[j], variants[i]);
+        fill_aux_fields(&m, get_matrix_with_sqrts(-1, variants[i]));
 
         fprintf(f, "Initial a:\n");
         print_2d_array_to_file(f, m.n, m.a);
@@ -111,16 +124,16 @@ void run_tests(struct method* method) {
         fprintf(f, "\nResidual c:\n");
         print_array_to_file(f,  m.n, m.c);
 
-        fprintf(f, "Matrix a after calculation:\n");
+        fprintf(f, "Part a after calculation:\n");
         print_2d_array_to_file(f, m.n, m.a);
 
-        fprintf(f, "Matrix b after calculation:\n");
+        fprintf(f, "Part b after calculation:\n");
         print_array_to_file(f,  m.n, m.b);
 
 
         free_matrix(&m);
-
         fclose(f);
+    }
     }
 }
 
